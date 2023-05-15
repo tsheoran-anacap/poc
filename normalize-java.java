763c7f90954c358aecd001f4620d8e32d9dbb791
tsheoran-anacap <96809290+tsheoran-anacap@users.noreply.github.com>
2023-05-15 11:19:30 +0530

    Update normalize-java.java

--- normalize-java.java
+++ normalize-java.java
@@ -1,48 +1,58 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class DatasetNormalization {
     public static void main(String[] args) {
-        int[][] dataset = {{10, 20, 30}, {55, 67, 90}, {100, 45, 98}};
-        double[][] normalizedDataset = normalizeDataset(dataset);
-        
+        List<List<Double>> dataset = new ArrayList<>();
+        dataset.add(Arrays.asList(10.0, 20.0, 30.0));
+        dataset.add(Arrays.asList(55.0, 67.0, 90.0));
+        dataset.add(Arrays.asList(100.0, 45.0, 98.0));
+
+        List<List<Double>> normalizedDataset = normalizeDataset(dataset);
+
         // Print normalized dataset
-        for (double[] point : normalizedDataset) {
-            System.out.println(Arrays.toString(point));
+        for (List<Double> point : normalizedDataset) {
+            System.out.println(point);
         }
     }
-    
-    public static double[][] normalizeDataset(int[][] dataset) {
-        int numDimensions = dataset[0].length;
-        
+
+    public static List<List<Double>> normalizeDataset(List<List<Double>> dataset) {
+        int numDimensions = dataset.get(0).size();
+
         // Find the maximum and minimum values for each dimension
-        int[] maxVals = new int[numDimensions];
-        int[] minVals = new int[numDimensions];
-        
-        Arrays.fill(maxVals, Integer.MIN_VALUE);
-        Arrays.fill(minVals, Integer.MAX_VALUE);
-        
-        for (int[] point : dataset) {
+        double[] maxVals = new double[numDimensions];
+        double[] minVals = new double[numDimensions];
+
+        Arrays.fill(maxVals, Double.MIN_VALUE);
+        Arrays.fill(minVals, Double.MAX_VALUE);
+
+        for (List<Double> point : dataset) {
             for (int i = 0; i < numDimensions; i++) {
-                if (point[i] > maxVals[i]) {
-                    maxVals[i] = point[i];
+                if (point.get(i) > maxVals[i]) {
+                    maxVals[i] = point.get(i);
                 }
-                
-                if (point[i] < minVals[i]) {
-                    minVals[i] = point[i];
+
+                if (point.get(i) < minVals[i]) {
+                    minVals[i] = point.get(i);
                 }
             }
         }
-        
+
         // Normalize the dataset
-        double[][] normalizedDataset = new double[dataset.length][numDimensions];
-        
-        for (int i = 0; i < dataset.length; i++) {
-            for (int j = 0; j < numDimensions; j++) {
-                double normalizedValue = (dataset[i][j] - minVals[j]) / (double)(maxVals[j] - minVals[j]);
-                normalizedDataset[i][j] = Math.round(normalizedValue * 10.0) / 10.0; // Round to one decimal place
+        List<List<Double>> normalizedDataset = new ArrayList<>();
+
+        for (List<Double> point : dataset) {
+            List<Double> normalizedPoint = new ArrayList<>();
+
+            for (int i = 0; i < numDimensions; i++) {
+                double normalizedValue = (point.get(i) - minVals[i]) / (maxVals[i] - minVals[i]);
+                normalizedPoint.add(Math.round(normalizedValue * 10.0) / 10.0); // Round to one decimal place
             }
+
+            normalizedDataset.add(normalizedPoint);
         }
-        
+
         return normalizedDataset;
     }
 }
